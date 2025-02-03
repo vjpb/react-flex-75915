800bb5e0ecd5c3324397355d1cee9c0b6d997909
Victor Pedroza <vjpb881104@gmail.com>
2025-02-03 09:29:57 -0500

    chore: iniciamos proyecto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  let nombreDelEstudiante = "Vitiok";
+
+  return (
+  <h1>Bienvenido al curso y espero que aprendas mucho {nombreDelEstudiante}</h1>
+  )
+}
+
+export default App
